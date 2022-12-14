a7abad93c7bf64c30707531064b91795c4d5ad62
Manan Agrawal <agrawalmanan2@gmail.com>
2022-12-14 18:46:26 +0530

    initialCommit

---
+++ data.js
@@ -0,0 +1,319 @@
+const data = {
+    logoColor: "#fff",
+    popup__img__logo__1 : "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/sVy9QTw_F_c96n_Isv.png?ik-sdk-version=javascript-1.4.3&updatedAt=1657633157365" ,
+    popup__img__logo__2: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/sVy9QTw_F_c96n_Isv.png?ik-sdk-version=javascript-1.4.3&updatedAt=1657633157365",
+    header__logo__img__sm: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/sVy9QTw_F_c96n_Isv.png?ik-sdk-version=javascript-1.4.3&updatedAt=1657633157365",
+    header__logo__img: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/sVy9QTw_F_c96n_Isv.png?ik-sdk-version=javascript-1.4.3&updatedAt=1657633157365",
+    background_images: {
+        slider_img1: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_Elevation__XprM3gMzT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633901393",
+        slider_img2: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_Elevation__XprM3gMzT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633901393",
+        slider_img3: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_Elevation__XprM3gMzT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633901393"
+    },
+    gallery__slider: {
+        gallery__slider__img1: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0010_7t3BL3V-F.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633898972",
+        gallery__slider__img2: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0011_633gVevn0.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633899220",
+        gallery__slider__img3: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0012_F-oAayWna.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633899544",
+        gallery__slider__img4: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0007_NPXUAkGS3.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633897856",
+        gallery__slider__img5: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0008_DYysWCM6l.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633898014",
+        gallery__slider__img6: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0009_8dOztcxPD.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633898457",
+        gallery__slider__img7: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_2_PrHnKRb-v.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633901203",
+        gallery__slider__img8: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/0013_0YAbi3e0L.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633900362",
+        gallery__slider__img9: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_Elevation__XprM3gMzT.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633901393"
+
+    },
+    about: {
+        heading: "About the Builder – SNN Builders",
+        content: "Established in 1994, SNN Builders have completed over 40 projects covering more than 12 million sqft. SNN Builders property development portfolio is diverse in scope and style, attracting considerable interest from not just end consumers but also from many investors. The key competitive advantages are SNN’s sound financial backing, availability of large resources and high level of management expertise. \n \n SNN’s corporate philosophy is to focus on balancing the evolving needs of a modern city while maintaining and conserving the environment. Most of our ongoing & upcoming projects are located in South-East & North Bangalore with a range of 1,2,3,4 BHK."
+    },
+
+    amenities: {
+        amenities1: "Table Tennis",
+        amenities2: "Outer Door Court",
+        amenities3: "Gym",
+        amenities4: "Kids Play Area",
+        amenities5: "Infinity Pool",
+        amenities6: "Yoga",
+        amenities7: "Jogging Track",
+        amenities8: "Garden",
+        amenities9: "Spa",
+        amenities10: "Basketball Court",
+    },
+    sfp: {
+        sfp1: {
+            heading: "1 BHK",
+            img_url: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_1_Bhk_675_VHkpo1245.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1662119206746"
+        },
+        sfp2: {
+            heading: "2 BHK",
+            img_url: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/2_Bhk_965_X905uutJx.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1662121307023"
+        },
+        sfp3: {
+            heading: "2.5 BHK",
+            img_url: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/2.5_Bhk_1090_WShVCQ-i-.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1662122667379"
+        }
+    },
+    MasterPlan:{
+           mP: {
+               heading: "Master Plan",
+               img_url: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/WhatsApp_Image_2022-07-12_at_6.46.39_PM_FWz7Ims87.jpeg?ik-sdk-version=javascript-1.4.3&updatedAt=1657633332906"
+           } 
+    },
+    priceSection: [
+        {
+            type: "1 BHK",
+            rera_carpet: "675 Sq.ft",
+            price: "Price Breakup"
+        },
+        {
+            type: "2 BHK",
+            rera_carpet: "870 Sq.ft - 1000 Sq.ft",
+            price: "Price Breakup"
+        },
+
+        {
+            type: "3 BHK",
+            rera_carpet: "1090 Sq.ft - 1650 Sq.ft",
+            price: "Price Breakup"
+        },
+    
+    ],
+    newLaunch: {
+        heading: "SNN High Gardens",
+        content: "1, 2, 2.5 & 3 BHK Apartment",
+        btn__heading: "MOST AWAITED LAUNCH",
+        lines: {
+            line1: "Total Land- 9 Acres ",
+            line2: "2 Basement + G + 14 floors",
+            line3: "1050 Units",
+            line4: "",
+            line5: "",
+            line6: "",
+            line7: "",
+            line8: "",
+        }
+    },
+    overview: {
+        heading: "SNN High Gardens",
+        content: "SNN High Gardens is one unique boutique apartment consisting of 2 and 2.5 BHK. As we all know that traffic is increasing day by day and the cost of the apartment is soaring high. SNN High Gardens is one such apartment which is located very close to Hosur. It's difficult for all looking to buy affordable apartments in East Bangalore but here we are with an excellent option for you to buy. SNN High Gardens residences apartment Neralur, Chandapura is being offered at the best price for one to choose from the various options available. \n \n SNN High Gardens 1.5, 2 & 2.5 Apartments for sale will be a peaceful community but still be close to all major requirements like school, college, hospital, and most importantly, work locations. SNN High Gardens is close to all major work locations of Bangalore. SNN High Gardens is a community where you can easily reside with your family and take advantage of all modern day amenities for day-to-day living. It is a property which is very thoughtfully designed so that all it home buyers have their privacy as well as enjoy the amenities in the property. The main objective of the company is to provide luxury peaceful living to all its home buyers staying close to their work locations. This is a very difficult combination. Most of the time a home buyer will get a property which will match all its requirements but will be far from its work location, or they will get a property which will be close to its work location but will not match their requirement. Here with SNN High Gardens residences from SNN Developers is one such option which is also close to office and also has all the features which every home buyer requires. Its property which will give you the comfort of living and also save a lot of time traveling to work locations and important destinations. Bangalore is growing immensely and is changing into a more cosmopolitan society. The demand for apartments is soaring high and especially in prime locations. SNN High Gardens residences fall in this bracket and that is why one should quickly decide to pick it up at the earliest. It is the right time if someone wants to stay in a prime location, he can look at SNN High Gardens and pick the unit at the earliest without paying extra, as demand for the property is very high.SNN RAJ HIGH GARDENS, another landmark project by SNN RAJ Corp is located in just ahead of chandapura circle. Located BANG On the 8 LANE HIGHWAY this project boasts excellent connectivity."
+    },
+    location: {
+        location__map: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d124506.30365878198!2d77.733461!3d12.790064!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xdd30a722a1d728aa!2sSnn%20Raj%20High%20Gardens!5e0!3m2!1sen!2sin!4v1657632882608!5m2!1sen!2sin",
+        location__map__img: "https://ik.imagekit.io/hp4i1dxyi/Snn_Raj_High_garden/SNN_RAJ_HIGH_GADRENS_BROCHURE_page-0004_xylfYCmHI.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1657634111459",
+        location__content: "Bangalore, also known as Bengaluru (Kannada) is the capital of the Indian State of Karnataka. Bangalore is nicknamed the Garden City and was once called a Pensioner's Paradise. Located on the Deccan Plateau in the south-eastern part of Karnataka, Bangalore is India's third most populous city. There are a number of expats from across the world living in the city, thanks to the growing presence of Multi-National Companies.",
+        nearby: {
+            nearby1: "STRR (Satellite town ring road - 5 Minutes",
+            nearby2: "PRR (Peripheral ring road - 10 minutes",
+            nearby3: "Ebenezer international school - 15 minutes",
+            nearby4: "Upcoming M5 Mall - 10 minutes",
+            nearby5: "2 temples in the vicinity",
+            nearby6: "Upcoming sobha mall - 1 minute (Walking distance)",
+            nearby7: "E city toll - 14 min",
+            nearby8: "Bommasandra metro - 10 minutes",
+            nearby9: "Hosur Airport - 30 minutes",
+            nearby10: "Jigani - 25 minutes",
+            nearby11: "Tvs factory- 19 minutes",
+            nearby12: "Sipcot 1 and 2 - 15-20 minutes",
+        }
+    }
+}
+
+
+
+let newLaunch__btn__heading = document.getElementById("newLaunch__btn__heading")
+newLaunch__btn__heading.innerText = data.newLaunch.btn__heading
+
+let newLaunch__line1 = document.getElementById("newLaunch__line1")
+newLaunch__line1.innerText = data.newLaunch.lines.line1
+
+let newLaunch__line2 = document.getElementById("newLaunch__line2")
+newLaunch__line2.innerText = data.newLaunch.lines.line2
+
+let newLaunch__line3 = document.getElementById("newLaunch__line3")
+newLaunch__line3.innerText = data.newLaunch.lines.line3
+
+let newLaunch__line4 = document.getElementById("newLaunch__line4")
+newLaunch__line4.innerText = data.newLaunch.lines.line4
+
+let newLaunch__line5 = document.getElementById("newLaunch__line5")
+newLaunch__line5.innerText = data.newLaunch.lines.line5
+
+let newLaunch__line6 = document.getElementById("newLaunch__line6")
+newLaunch__line6.innerText = data.newLaunch.lines.line6
+
+let newLaunch__line7 = document.getElementById("newLaunch__line7")
+newLaunch__line7.innerText = data.newLaunch.lines.line7
+
+let newLaunch__line8 = document.getElementById("newLaunch__line8")
+newLaunch__line8.innerText = data.newLaunch.lines.line8
+
+
+let gallery__slider__img1 = document.getElementById("gallery__slider__img1")
+gallery__slider__img1.src = data.gallery__slider.gallery__slider__img1
+
+let gallery__slider__img2 = document.getElementById("gallery__slider__img2")
+gallery__slider__img2.src = data.gallery__slider.gallery__slider__img2
+
+let gallery__slider__img3 = document.getElementById("gallery__slider__img3")
+gallery__slider__img3.src = data.gallery__slider.gallery__slider__img3
+
+let popup__img__logo__1 = document.getElementById("popup__img__logo__1")
+popup__img__logo__1.src = data.popup__img__logo__1
+
+let popup__img__logo__2 = document.getElementById("popup__img__logo__2")
+popup__img__logo__2.src = data.popup__img__logo__2
+
+let location__map = document.getElementById("location__map")
+location__map.src = data.location.location__map
+
+let location__map__img = document.getElementById("location__map__img")
+location__map__img.src = data.location.location__map__img
+
+let header__logo__img = document.getElementById("header__logo__img")
+header__logo__img.src = data.header__logo__img__sm
+
+let header__logo__img__sm = document.getElementById("header__logo__img__sm")
+header__logo__img__sm.src = data.header__logo__img__sm
+
+let slider_img1 = document.getElementById("slider_img1")
+slider_img1.src = data.background_images.slider_img1
+
+let slider_img2 = document.getElementById("slider_img2")
+slider_img2.src = data.background_images.slider_img2
+
+let slider_img3 = document.getElementById("slider_img3")
+slider_img3.src = data.background_images.slider_img3
+
+let slider_img1_responsive = document.getElementById("slider_img1_responsive")
+slider_img1_responsive.src = data.background_images.slider_img1
+
+let slider_img2_responsive = document.getElementById("slider_img2_responsive")
+slider_img2_responsive.src = data.background_images.slider_img2
+
+let slider_img3_responsive = document.getElementById("slider_img3_responsive")
+slider_img3_responsive.src = data.background_images.slider_img3
+
+
+let sfp1__heading = document.getElementById("sfp1__heading")
+sfp1__heading.innerText = data.sfp.sfp1.heading
+
+let sfp1__img = document.getElementById("sfp1__img")
+sfp1__img.src = data.sfp.sfp1.img_url
+
+let sfp2__heading = document.getElementById("sfp2__heading")
+sfp2__heading.innerText = data.sfp.sfp2.heading
+
+let sfp2__img = document.getElementById("sfp2__img")
+sfp2__img.src = data.sfp.sfp2.img_url
+
+let sfp3__heading = document.getElementById("sfp3__heading")
+sfp3__heading.innerText = data.sfp.sfp3.heading
+
+let sfp3__img = document.getElementById("sfp3__img")
+sfp3__img.src = data.sfp.sfp3.img_url
+
+let mP__img = document.getElementById("mP__img")
+mP__img.src = data.MasterPlan.mP.img_url
+
+let location__content = document.getElementById("location__content")
+location__content.innerText = data.location.location__content
+
+
+let location__nearby1 = document.getElementById("location__nearby1")
+location__nearby1.innerText = data.location.nearby.nearby1
+
+
+let location__nearby2 = document.getElementById("location__nearby2")
+location__nearby2.innerText = data.location.nearby.nearby2
+
+let location__nearby3 = document.getElementById("location__nearby3")
+location__nearby3.innerText = data.location.nearby.nearby3
+
+let location__nearby4 = document.getElementById("location__nearby4")
+location__nearby4.innerText = data.location.nearby.nearby4
+
+let location__nearby5 = document.getElementById("location__nearby5")
+location__nearby5.innerText = data.location.nearby.nearby5
+
+let location__nearby6 = document.getElementById("location__nearby6")
+location__nearby6.innerText = data.location.nearby.nearby6
+let location__nearby7 = document.getElementById("location__nearby7")
+location__nearby7.innerText = data.location.nearby.nearby7
+let location__nearby8 = document.getElementById("location__nearby8")
+location__nearby8.innerText = data.location.nearby.nearby8
+let location__nearby9 = document.getElementById("location__nearby9")
+location__nearby9.innerText = data.location.nearby.nearby9
+let location__nearby10 = document.getElementById("location__nearby10")
+location__nearby10.innerText = data.location.nearby.nearby10
+let location__nearby11 = document.getElementById("location__nearby11")
+location__nearby11.innerText = data.location.nearby.nearby11
+let location__nearby12 = document.getElementById("location__nearby12")
+location__nearby12.innerText = data.location.nearby.nearby12
+
+let overview__heading = document.getElementById("overview__heading")
+overview__heading.innerText = data.overview.heading
+
+
+let overview__content = document.getElementById("overview__content")
+overview__content.innerText = data.overview.content
+
+
+let ps__type1 = document.getElementById("ps__type1")
+ps__type1.innerText = data.priceSection[0].type
+
+let ps__rc1 = document.getElementById("ps__rc1")
+ps__rc1.innerText = data.priceSection[0].rera_carpet
+
+let ps__type2 = document.getElementById("ps__type2")
+ps__type2.innerText = data.priceSection[1].type
+
+let ps__rc2 = document.getElementById("ps__rc2")
+ps__rc2.innerText = data.priceSection[1].rera_carpet
+
+let ps__type3 = document.getElementById("ps__type3")
+ps__type3.innerText = data.priceSection[2].type
+
+let ps__rc3 = document.getElementById("ps__rc3")
+ps__rc3.innerText = data.priceSection[2].rera_carpet
+
+
+let newLaunch__heading = document.getElementById("newLaunch__heading")
+newLaunch__heading.innerText = data.newLaunch.heading
+
+let newLaunch__content = document.getElementById("newLaunch__content")
+newLaunch__content.innerText = data.newLaunch.content
+
+let about__heading = document.getElementById("about__heading")
+about__heading.innerText = data.about.heading
+
+let about__content = document.getElementById("about__content")
+about__content.innerText = data.about.content
+
+let amenities1 = document.getElementById("amenities1")
+amenities1.innerText = data.amenities.amenities1
+
+let amenities2 = document.getElementById("amenities2")
+amenities2.innerText = data.amenities.amenities2
+
+let amenities3 = document.getElementById("amenities3")
+amenities3.innerText = data.amenities.amenities3
+
+let amenities4 = document.getElementById("amenities4")
+amenities4.innerText = data.amenities.amenities4
+
+let amenities5 = document.getElementById("amenities5")
+amenities5.innerText = data.amenities.amenities5
+
+let amenities6 = document.getElementById("amenities6")
+amenities6.innerText = data.amenities.amenities6
+
+let amenities7 = document.getElementById("amenities7")
+amenities7.innerText = data.amenities.amenities7
+
+let amenities8 = document.getElementById("amenities8")
+amenities8.innerText = data.amenities.amenities8
+
+let amenities9 = document.getElementById("amenities9")
+amenities9.innerText = data.amenities.amenities9
+
+let amenities10 = document.getElementById("amenities10")
+amenities10.innerText = data.amenities.amenities10
